0ee0b07a17cbb50d0375405aafd680e5653f4733
shrikantbankar <shrikantbankar1998@gmail.com>
2022-01-02 23:32:26 +0530

    wpt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+export default function App() {
+  let username = "ROHIT";
+  let cnt = 100;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> {username}</h1>
+      <h1> {cnt}</h1>
+      <h1> aditya</h1>
+      <h1> shrikant</h1>
+      <h1> shridhar</h1>
     </div>
   );
 }
-
-export default App;
